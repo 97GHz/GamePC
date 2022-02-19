998344b4aa6d76961d96d77490b580fe8943e6af
SeungYeop Han <tmdduq525@naver.com>
2022-02-20 00:41:58 +0900

    feat: User Domain 생성
    User Domain에 대한 Entity 생성
    User에 할당된 Enum Role 구현 완료

---
+++ src/main/java/com/gpsiu/gamepc/domain/Member.java
@@ -0,0 +1,28 @@
+package com.gpsiu.gamepc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/gpsiu/gamepc/domain/Role.java
@@ -0,0 +1,5 @@
+package com.gpsiu.gamepc.domain;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
