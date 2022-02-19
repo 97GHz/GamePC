b82f02ae486d4dd3e33cbdee2f6148f03cc714d1
SeungYeop Han <tmdduq525@naver.com>
2022-02-19 21:52:14 +0900

    feat: Setup Project
    Setup Project
     - Dependency
    
    Resolves: None

---
+++ src/main/java/com/gpsiu/gamepc/GamepcApplication.java
@@ -0,0 +1,13 @@
+package com.gpsiu.gamepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpsiu/gamepc/GamepcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpsiu.gamepc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
